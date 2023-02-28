0051bb265b7d98bc0cfc1070d78f17d72996066d
Nick <nick.buscemi13@gmail.com>
2023-02-28 13:08:33 -0500

    Finished

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,46 +1,78 @@
 class DynamicArray {
 
   constructor(defaultSize = 4) {
-
-    // Your code here
+    this.capacity = defaultSize;
+    this.length = 0;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   push(val) {
-
-    // Your code here
+    if (this.length === this.capacity) {
+      this.resize();
+    }
+    this.data[this.length++] = val;
   }
 
 
   pop() {
-
-    // Your code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    const poppedValue = this.data[--this.length];
+    this.data[this.length] = undefined;
+    if (this.length <= this.capacity / 2) {
+      this.resize();
+    }
+    return poppedValue;
   }
 
   shift() {
-
-    // Your code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    const shiftedValue = this.data[0];
+    for (let i = 0; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    this.data[--this.length] = undefined;
+    if (this.length <= this.capacity / 2) {
+      this.resize();
+    }
+    return shiftedValue;
   }
 
   unshift(val) {
-
-    // Your code here
+    if (this.length === this.capacity) {
+      this.resize();
+    }
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf(val) {
-
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   resize() {
-
-    // Your code here
+    const newData = new Array(this.capacity * 2);
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+    this.data = newData;
+    this.capacity *= 2;
   }
-
 }
 
 
